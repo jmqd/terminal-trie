2f1ff801d95f520207b43346e564fb2db91921a1
Jordan McQueen <dojo3@uw.edu>
2017-04-30 00:34:29 -0700

    making shebang line generated at install time

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 mkdir -p ~/.tt
 cp -r tt/* ~/.tt/
+py_bin=`which python3`
+shebang="#!$py_bin"
+exec 3<> ~/.tt/main.py && awk -v TEXT="$shebang" 'BEGIN {print TEXT}{print}' ~/.tt/main.py >&3
 echo "Successfully installed tt. Config and app is in ~/tt/ directory."
+
